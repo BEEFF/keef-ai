7ef6c5a39ebb28525b31578a979edb44acff05a5
Thomas O'Keeffe <thomas.okeeffe@opsview.com>
2019-07-24 16:48:13 +0100

    Added cards dataset

--- tensorflow.py
+++ tensorflow.py
@@ -1,15 +1,15 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import tensorflow as tf
-from tensorflow import keras
+#from tensorflow import keras
 # Helper libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import logging
 logging.getLogger('tensorflow').disabled = True
+from google_images_download import google_images_download   #importing the library
 
 def print_sep():
     print("--------------------------------------")
-    
 
 def fashion():
     fashion_mnist = keras.datasets.fashion_mnist
@@ -90,17 +90,22 @@ def fashion():
     predicted_label = (np.argmax(predictions[0])) #get the maximum probability of the image belonging to a specific class
     print(class_names[predicted_label])
 
-def organise_playing_cards():
+def get_card_names():
+    suits = ["Hearts,", "Clubs", "Diamonds", "Spades"]
+    numbers = ["Ace", "King", "Queen", "Jack", "Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two"]
+    terms = []
+    for number in numbers:
+        for suit in suits:
+            terms.append(number + " of " + suit + " card")
+    return terms
 
+def get_google_images(search_term, number=5):
+    response = google_images_download.googleimagesdownload()   #class instantiation
+    arguments = {"keywords":search_term,"limit":number,"print_urls":False}   #creating list of arguments
+    paths = response.download(arguments)   #passing the arguments to the function
 
-
-
-
-
-
-
-
-
-
-
-
+def scrape_cards_data():
+    card_names = get_card_names()
+    for card in card_names:
+        get_google_images(card, 15)
+scrape_cards_data()
---
+++ tensorflow_cards.py
@@ -0,0 +1,38 @@
+## Developed by Thomas O'Keeffe #
+## Modules ##
+from google_images_download import google_images_download
+import tensorflow as tf
+#from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+import logging
+from PIL import Image
+
+def get_card_names():
+    suits = ["Hearts,", "Clubs", "Diamonds", "Spades"]
+    numbers = ["Ace", "King", "Queen", "Jack", "Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two"]
+    terms = []
+    for number in numbers:
+        for suit in suits:
+            terms.append(number + " of " + suit + " card")
+    return terms
+
+def get_google_images(search_term, number=5):
+    response = google_images_download.googleimagesdownload()   #class instantiation
+    arguments = {"keywords":search_term,"limit":number,"print_urls":False}   #creating list of arguments
+    paths = response.download(arguments)   #passing the arguments to the function
+
+def scrape_cards_data():
+    card_names = get_card_names()
+    for card in card_names:
+        get_google_images(card, 15)
+
+def image_resize():
+    basewidth = 28 # MNIST image width
+    img = Image.open('your_input_img.jpg')
+    wpercent = (basewidth/float(img.size[0]))
+    hsize = int((float(img.size[1])*float(wpercent)))
+    img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+
+def training():
+    class_labels = get_card_names()
